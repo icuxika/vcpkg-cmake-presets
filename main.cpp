5a6f4c7a72a404595c9308fb7022330f73ae2e04
icuxika <icuxika@outlook.com>
2023-09-13 20:46:47 +0800

    .clang-format

--- main.cpp
+++ main.cpp
@@ -5,28 +5,29 @@
 namespace view = ranges::views;
 
 int fib(int x) {
-  int a = 0, b = 1;
+	int a = 0, b = 1;
 
-  for (int it : view::repeat(0) | view::take(x)) {
-    (void)it;
-    int tmp = a;
-    a += b;
-    b = tmp;
-  }
-
-  return a;
+	for (int it : view::repeat(0) | view::take(x)) {
+		(void)it;
+		int tmp = a;
+		a += b;
+		b = tmp;
+	}
+	return a ;
 }
 
 int main(int argc, char **argv) {
-  cxxopts::Options options("fibo",
-                           "Print the fibonacci sequence up to a value 'n'");
-  options.add_options()("n,value", "The value to print to",
-                        cxxopts::value<int>()->default_value("10"));
+	cxxopts::Options options(
+		"fibo", "Print the fibonacci sequence up to a value 'n'");
+	options.add_options()("n,value", "The value to print to",
+		cxxopts::value<int>()->default_value("10"));
 
-  auto result = options.parse(argc, argv);
-  auto n = result["value"].as<int>();
+	auto result = options.parse(argc, argv);
+	auto n = result["value"].as<int>();
 
-  for (int x : view::iota(1) | view::take(n)) {
-    fmt::print("fib({}) = {}\n", x, fib(x));
-  }
+	for (int x : view::iota(1) | view::take(n)) {
+		fmt::print("fib({}) = {}\n", x, fib(x));
+	}
+
+  return 0;
 }
