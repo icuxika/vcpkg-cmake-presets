49416964c2a456c3ceae9c858bea9059a929a379
icuxika <icuxika@outlook.com>
2024-12-11 13:37:11 +0800

    disbale clang-tidy on header file

--- SkiaUI/include/framework.h
+++ SkiaUI/include/framework.h
@@ -6,14 +6,17 @@
 
 #include "targetver.h"
 #define WIN32_LEAN_AND_MEAN // Exclude rarely-used stuff from Windows headers
-#define NOMINMAX
+// 更改头文件的排序会导致导入问题
+// NOLINTBEGIN
 // Windows Header Files
-#include <minidumpapiset.h>
-#include <pathcch.h>
+#define NOMINMAX
 #include <windows.h>
 #include <windowsx.h>
+#include <minidumpapiset.h>
+#include <pathcch.h>
 // C RunTime Header Files
 #include <malloc.h>
 #include <memory.h>
 #include <stdlib.h>
 #include <tchar.h>
+// NOLINTEND
